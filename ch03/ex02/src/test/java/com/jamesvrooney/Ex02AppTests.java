7c1ff25f7903dcb0ac1ad36cdef9fa9bb2990583
jamesvrooney <jamesvrooney@hotmail.com>
2022-07-26 16:12:43 +0100

    Completed example 2.

--- ch03/ex02/src/test/java/com/jamesvrooney/Ex02AppTests.java
+++ ch03/ex02/src/test/java/com/jamesvrooney/Ex02AppTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Ex02ApplicationTests {
+class Ex02AppTests {
 
 	@Test
 	void contextLoads() {
