5a405706657358911498b9d13bfe5919d3d7f180
jamesvrooney <jamesvrooney@hotmail.com>
2022-07-26 15:26:54 +0100

    Completed example 1.

---
+++ ch03/ex01/src/main/java/com/jamesvrooney/controllers/HelloController.java
@@ -0,0 +1,13 @@
+package com.jamesvrooney.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello() {
+        return "Hello Chapter 3 - example 1";
+    }
+}
--- ch03/ex01/src/test/java/com/jamesvrooney/Ex01AppTests.java
+++ ch03/ex01/src/test/java/com/jamesvrooney/Ex01AppTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Ex01ApplicationTests {
+class Ex01AppTests {
 
 	@Test
 	void contextLoads() {
